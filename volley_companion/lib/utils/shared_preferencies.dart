203fcc28f121fa6749ca74a8adaebe0b77931889
Alexandre Collin <alexandre.collin-betheuil@epitech.eu>
2022-09-05 02:13:30 +0200

    fix: restart project

--- volley_companion/lib/utils/shared_preferencies.dart
+++
@@ -1,9 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class SharedPreferencies {
-  static SharedPreferences? prefs;
-
-  Future<void> init() async {
-    prefs = await SharedPreferences.getInstance();
-  }
-}
